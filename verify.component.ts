428d317f1d782ec28f8db33e109e44127ae134b5
sanjana-loyalytics <60343551+sanjana-loyalytics@users.noreply.github.com>
2020-02-03 17:38:28 +0530

    Verify- email component

---
+++ verify.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'email-verify',
+  templateUrl: './verify.component.html',
+  styleUrls: ['./verify.component.css']
+})
+export class VerifyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
